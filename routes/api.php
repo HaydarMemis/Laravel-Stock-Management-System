48248a37c0d195b6944732b6ad80a3b1dcc2d6c0
Haydar Memis <haydarmemiiss@gmail.com>
2024-08-12 15:26:51 +0300

    first push

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+ 
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+ 
+    return ['token' => $token->plainTextToken];
+});
+ 
+
+
